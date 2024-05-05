798aca72af07ca059f7b208ecfc6f714316016ce
zelpro-dev <brawlstarszelpro@gmail.com>
2024-05-05 14:20:35 +0200

    aborts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import './App.css';
-import backgroundImage from './bg.jpg';
+import backgroundImage from './assets/bg.jpg';
+import captura1 from './assets/cap.png';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,6 +17,11 @@ function App() {
       <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
       <h1 className="title">El Mejor Bot De Discord</h1>
       <button className='button'><a href='#'>Añadir</a></button>
+      <section className='feature'>
+        <h1>Característica #1</h1>
+        <p>Gran facilidad para configurar.</p>
+        <img src={captura1} alt='Discover' />
+      </section>
     </div>
   );
 }
